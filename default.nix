7c0bc8061d7945dab8d217796406eef39b407e29
Jörg Thalheim <joerg@thalheim.io>
2020-12-02 16:54:01 +0000

    add tracedump

--- default.nix
+++ default.nix
@@ -1,4 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8eb28a1d2bc4358188845cb8a788f6d465aa55d6.tar.gz") {} }:
+let
+  tracedump = pkgs.callPackage (pkgs.fetchzip {
+    url = "https://github.com/Mic92/tracedump/archive/cf4ec3fd5b9ccbd347054cefdcfd955f5056c44a.tar.gz";
+    sha256 = "sha256-k/QeYUSBOGzmMRshq+9SJOmPDOfIPEb3va+p76252JU=";
+  });
+in
 (pkgs.buildFHSUserEnv {
   name = "parsec";
   targetPkgs = pkgs: (with pkgs; [
@@ -27,6 +33,10 @@
     # as noted in the README, x264 crashes due to double free.
     # However one can use the upstream version
     x264
+    jq
+    (python3.withPackages (py: [
+      (tracedump { inherit pkgs; inherit (py) python; })
+    ]))
   ]);
   # for ocean_ncp this seems to be not defined correctly
   profile = pkgs.lib.optionalString (pkgs.stdenv.is64bit) ''
